fe87bffee4e7aff42f9a7a24da8aed48f695abf0
helen-jin8 <143369682+helen-jin8@users.noreply.github.com>
2024-02-12 19:40:34 -0500

    copy from other repo's code

--- cnn.py
+++ cnn.py
@@ -1,23 +1,184 @@
-from keras.layers import Dense, Conv2D, MaxPool2D, Dropout, Flatten
-import logging
-from keras.models import Sequential
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+from torchvision import datasets, models, transforms
+from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import sampler, RandomSampler, SubsetRandomSampler
+from torch.utils.tensorboard import SummaryWriter
+from PIL import Image, ImageOps
+import torchvision.datasets as dset
+import torchvision.transforms as T
+
+import numpy as np
+from sklearn.metrics import confusion_matrix
+import time
+
+import seaborn as sns
+#from __future__ import print_function 
+#from __future__ import division
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
 
 
-logger = logging.getLogger(__name__)
+# Below is a copy and paste from shared git damaged_structures_detector !!!
 
 
-def build(input_shape, num_classes, filters, kernel_size, pool_size, padding, r_dropout):
-    return Sequential([
-        Conv2D(filters[0], kernel_size, padding=padding, input_shape=input_shape, activation='relu', name='conv1_1'),
-        Conv2D(filters[0], kernel_size, activation='relu', name='conv1_2'),
-        MaxPool2D(pool_size, name='pool1'),
-        Dropout(r_dropout, name='dropout1'),
-        Conv2D(filters[1], kernel_size, padding=padding, activation='relu', name='conv2_1'),
-        Conv2D(filters[1], kernel_size, activation='relu', name='conv2_2'),
-        MaxPool2D(pool_size, name='pool2'),
-        Dropout(r_dropout, name='dropout2'),
-        Flatten(),
-        Dense(512, activation='relu'),
-        Dropout(r_dropout, name='dropout3'),
-        Dense(num_classes, activation='softmax')
-    ])
+def set_parameter_requires_grad(model, feature_extracting):
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
+
+
+def initialize_model(model_name, num_classes, width, channels, feature_extract, use_pretrained=True):
+    # Initialize these variables which will be set in this if statement. Each of these
+    #   variables is model specific.
+    model_ft = None
+    input_size = 0
+
+    if model_name == "resnet":
+        """ Resnet18
+        """                
+        model_ft = models.resnet18(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+        if(width != input_size):       
+          W1 = width
+          if (input_size > W1):
+            F = 1
+            P = int((input_size - W1) / 2)
+          else:
+            P = 0
+            F = W1 - input_size +1
+          
+          first_conv_layer = nn.Conv2d(channels, 3, kernel_size=F, stride=1, padding=P, dilation=1, groups=1, bias=True)
+          model_ft = nn.Sequential(first_conv_layer, model_ft)
+  
+
+    elif model_name == "alexnet":
+        """ Alexnet
+        """
+        model_ft = models.alexnet(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier[6].in_features
+        model_ft.classifier[6] = nn.Linear(num_ftrs,num_classes)
+        input_size = 224
+
+        if(width != input_size):       
+          W1 = width
+          if (input_size > W1):
+            F = 1
+            P = int((input_size - W1) / 2)
+          else:
+            P = 0
+            F = W1 - input_size +1
+          
+          first_conv_layer = [nn.Conv2d(channels, 3, kernel_size=F, stride=1, padding=P, dilation=1, groups=1, bias=True)]
+          first_conv_layer.extend(list(model_ft.features))  
+          model_ft.features= nn.Sequential(*first_conv_layer)
+
+    elif model_name == "vgg":
+        """ VGG11_bn
+        """
+        model_ft = models.vgg11_bn(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier[6].in_features
+        model_ft.classifier[6] = nn.Linear(num_ftrs,num_classes)
+        input_size = 224
+
+        if(width != input_size):       
+          W1 = width
+          if (input_size > W1):
+            F = 1
+            P = int((input_size - W1) / 2)
+          else:
+            P = 0
+            F = W1 - input_size +1
+          
+          first_conv_layer = [nn.Conv2d(channels, 3, kernel_size=F, stride=1, padding=P, dilation=1, groups=1, bias=True)]
+          first_conv_layer.extend(list(model_ft.features))  
+          model_ft.features= nn.Sequential(*first_conv_layer)
+
+    elif model_name == "squeezenet":
+        """ Squeezenet
+        """
+        model_ft = models.squeezenet1_0(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        model_ft.classifier[1] = nn.Conv2d(512, num_classes, kernel_size=(1,1), stride=(1,1))
+        model_ft.num_classes = num_classes
+        input_size = 224
+
+        if(width != input_size):       
+          W1 = width
+          if (input_size > W1):
+            F = 1
+            P = int((input_size - W1) / 2)
+          else:
+            P = 0
+            F = W1 - input_size +1
+          
+          first_conv_layer = [nn.Conv2d(channels, 3, kernel_size=F, stride=1, padding=P, dilation=1, groups=1, bias=True)]
+          first_conv_layer.extend(list(model_ft.features))  
+          model_ft.features= nn.Sequential(*first_conv_layer)
+        
+
+    elif model_name == "densenet":
+        """ Densenet
+        """
+        model_ft = models.densenet121(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier.in_features
+        model_ft.classifier = nn.Linear(num_ftrs, num_classes) 
+        input_size = 224
+
+        if(width != input_size):       
+          W1 = width
+          if (input_size > W1):
+            F = 1
+            P = int((input_size - W1) / 2)
+          else:
+            P = 0
+            F = W1 - input_size +1
+          
+          first_conv_layer = [nn.Conv2d(channels, 3, kernel_size=F, stride=1, padding=P, dilation=1, groups=1, bias=True)]
+          first_conv_layer.extend(list(model_ft.features))  
+          model_ft.features= nn.Sequential(*first_conv_layer)
+
+    elif model_name == "inception":
+        """ Inception v3 
+        Be careful, expects (299,299) sized images and has auxiliary output
+        """
+        model_ft = models.inception_v3(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        # Handle the auxilary net
+        num_ftrs = model_ft.AuxLogits.fc.in_features
+        model_ft.AuxLogits.fc = nn.Linear(num_ftrs, num_classes)
+        # Handle the primary net
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs,num_classes)
+        input_size = 299
+
+        if(width != input_size):       
+          W1 = width
+          if (input_size > W1):
+            F = 1
+            P = int((input_size - W1) / 2)
+          else:
+            P = 0
+            F = W1 - input_size +1
+          
+          first_conv_layer = nn.Conv2d(channels, 3, kernel_size=F, stride=1, padding=P, dilation=1, groups=1, bias=True)
+          model_ft.AuxLogits = nn.Sequential(first_conv_layer, model_ft.AuxLogits)
+          model_ft = nn.Sequential(first_conv_layer, model_ft)
+
+
+    else:
+        print("Invalid model name, exiting...")
+        exit()
+    
+    return model_ft, input_size
